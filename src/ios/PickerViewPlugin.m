e6fc0926522ea325020170f0d6d09795635c4cd9
Suguru Murakami <sugumura@gmail.com>
2015-09-23 09:30:51 +0900

    fix PickerView layout

--- src/ios/PickerViewPlugin.m
+++ src/ios/PickerViewPlugin.m
@@ -26,7 +26,7 @@
     _providerToolbar.barStyle = UIBarStyleDefault;
     [view addSubview:_providerToolbar];
 
-    _providerPickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0, view.bounds.size.height - 215, view.bounds.size.width, 0)];
+    _providerPickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0, view.bounds.size.height - (260 - 44), view.bounds.size.width, 260)];
     _providerPickerView.backgroundColor = [UIColor colorWithRed:210.0f/255.0f green:213.0f/255.0f blue:219.0f/255.0f alpha:1.0];
     _providerPickerView.showsSelectionIndicator = YES;
     _providerPickerView.delegate = self;
